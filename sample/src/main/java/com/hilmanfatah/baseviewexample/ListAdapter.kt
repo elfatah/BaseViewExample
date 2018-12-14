6734d7d92d321c784a47ded39dba7d6e54b91c6c
subh2020 <subh2020@gmail.com>
2018-12-14 11:25:22 +0530

    Added Multi View Support This is just as example how to support multiple view types and can be extended for more complex example.

--- sample/src/main/java/com/hilmanfatah/baseviewexample/ListAdapter.kt
+++ sample/src/main/java/com/hilmanfatah/baseviewexample/ListAdapter.kt
@@ -1,15 +1,41 @@
 package com.hilmanfatah.baseviewexample
 
 import android.content.Context
+import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import com.hilmanfatah.baselistadapter.BaseRecyclerViewAdapter
+import com.hilmanfatah.baselistadapter.viewholder.BaseItemViewHolder
 
 /**
  * Created by Hilman N Fatah on 30/01/18.
  */
-class ListAdapter(context: Context) : BaseRecyclerViewAdapter<UserModel, UserViewHolder>(context) {
-    override fun getItemResourceLayout(viewType: Int): Int = R.layout.user_item_view
+class ListAdapter(context: Context) : BaseRecyclerViewAdapter<Any, BaseItemViewHolder<Any>>(context) {
+    override fun getItemResourceLayout(viewType: Int): Int {
+        when (viewType)
+        {
+            0 -> return R.layout.user_item_view
+            1 -> return R.layout.user_item_view2
+            else -> return -1
+        }
+    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder =
-            UserViewHolder(getView(parent!!, viewType), mItemClickListener, mLongItemClickListener)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseItemViewHolder<Any> {
+        when (viewType)
+        {
+            0 -> return UserViewHolder(getView(parent, viewType), mItemClickListener, mLongItemClickListener)
+            1 -> return UserViewHolder2(getView(parent, viewType), mItemClickListener, mLongItemClickListener)
+            else -> throw IllegalArgumentException("Valid View Type required")
+        }
+
+    }
+
+
+    override fun getItemViewType(position: Int): Int {
+        if (mDatas.get(position) is UserModel) {
+            return 0
+        } else if (mDatas.get(position) is UserModel2) {
+            return 1
+        } else
+            return -1
+    }
 }
